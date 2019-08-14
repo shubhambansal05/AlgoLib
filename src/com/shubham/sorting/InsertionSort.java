dff2fb63e5cb8c3124e80267f38220d622122d57
shubham <shubhambansal05@gmail.com>
2019-08-14 05:39:18 +0530

    Adding binaryInsertionSort to InsertionSort.

--- src/com/shubham/sorting/InsertionSort.java
+++ src/com/shubham/sorting/InsertionSort.java
@@ -1,10 +1,12 @@
 package com.shubham.sorting;
 
+import java.util.Arrays;
+
 public class InsertionSort implements Sort {
 
     @Override
     public void sort(int[] numbersArr) {
-        insertionSort(numbersArr, 0, numbersArr.length - 1);
+        binaryInsertionSort(numbersArr, 0, numbersArr.length - 1);
     }
 
     public void insertionSort(int[] numbersArr, int start, int end) {
@@ -14,5 +16,14 @@ public class InsertionSort implements Sort {
                     swap(numbersArr, j + 1, j);
                 else
                     break;
+    }
+
+    public void binaryInsertionSort(int[] numbersArr, int start, int end) {
+        for (int i = start + 1; i <= end; i++) {
+            int number = numbersArr[i];
+            int index = Math.abs(Arrays.binarySearch(numbersArr, 0, i, number) + 1);
+            System.arraycopy(numbersArr, index, numbersArr, index + 1, i - index);
+            numbersArr[index] = number;
+        }
     }
 }
